40e0e03487b800fc394adb4a95c57dd0f851527c
noahho <Noah.homa@gmail.com>
2025-03-21 16:07:58 +0100

    - fixing tests

--- src/tabpfn_extensions/hpo/search_space.py
+++ src/tabpfn_extensions/hpo/search_space.py
@@ -169,7 +169,7 @@ def get_param_grid_hyperopt(task_type: str) -> dict:
         # Custom HPs
         "model_type": hp.choice(
             "model_type",
-            ["single"],  # TODO: Reenable "dt_pfn" when properly tested
+            ["single", "dt_pfn"],
         ),
         "n_ensemble_repeats": hp.choice("n_ensemble_repeats", [4]),
         "max_depth": hp.choice("max_depth", [2, 3, 4, 5]),  # For Decision Tree TabPFN
